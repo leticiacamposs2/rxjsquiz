f2765d42e9d349b1b9238d40b1958a2a90c39197
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2021-02-01 19:20:57 -0300

    finalizando o quiz

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 
@@ -34,7 +36,16 @@ export default function Home() {
       </Head>
       <QuizContainer>
         <QuizLogo />
-          <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >            
             <Widget.Header>
               <h1>{db.title}</h1>
             </Widget.Header>
@@ -61,11 +72,40 @@ export default function Home() {
             <Widget.Content>
               <h1>Quizes da Galera</h1>
 
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
+              <ul>
+              {db.external.map((linkExterno) => {
+                // const [projectName, githubUser] = linkExterno
+                //   .replace(/\//g, '')
+                //   .replace('https:', '')
+                //   .replace('.vercel.app', '')
+                //   .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    {linkExterno}
+                    {/* <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic> */}
+                  </li>
+                );
+              })}
+            </ul>
+          </Widget.Content>
           </Widget>
-        <Footer />
-      </QuizContainer>
+          <Footer
+            as={motion.footer}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            variants={{
+              show: { opacity: 1 },
+              hidden: { opacity: 0 },
+            }}
+            initial="hidden"
+            animate="show"
+          />
+        </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/leticiacamposs2" />
     </QuizBackground>
   );
